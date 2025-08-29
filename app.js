31ad0ddba628043fb31e3354c4832e1f34d1c177
ghazalababar16 <g.babar.khan@gmail.com>
2025-08-30 01:40:42 +0500

    add some

---
+++ app.js
@@ -0,0 +1,15 @@
+
+// 1. Write a script to greet your website visitor using JS alert box
+
+// 2. Write a script to display following message on your web page
+
+alert("error!please enter a valid password.")
+
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break) 
+
+alert("welcome to JS land happy coding")
+
+// 4. Write a script to display following messages in sequence:
+
+alert("welcome to js land")
