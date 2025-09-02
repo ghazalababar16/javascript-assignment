bb1c144522d9b45cea5f14bfa9903b41b3a788d4
ghazalababar16 <g.babar.khan@gmail.com>
2025-09-03 01:50:51 +0500

    complete

---
+++ assignment-02/app.js
@@ -0,0 +1,47 @@
+//  1. Declare a variable called username.
+// answer
+// var  studentName="ghazala babar"
+
+/*2. Declare a variable called myName & assign to it a string
+that represents your Full Name.*/
+// 
+/*console . log("studentName")
+console .log("ghazala babar")*/
+
+/*3. Write script to
+a) Declare a JS variable, titled message.
+b) Assign “Hello World” to variable message
+c) Display the message in alert box.*/
+
+// answer
+/*var massage="welcome to javascript"
+console.log ("massage")*/
+
+// var massage="hello world"
+// console.log ("hello world")
+
+// alert("welcome to javascript")
+
+/*4. Write a script to save student’s bio data in JS variables and
+show the data in alert boxes.*/
+// answer
+
+/*alert ("ghazala babar")
+alert(" 34 years old")
+alert("certified website development")*/
+
+/*5. Write a script to display the following alert using one JS
+variable:*/
+// answer
+
+alert("PIZZA \nPIZZ\nPIZ \nPI \nP")
+
+
+/*6. Declare a variable called email and assign to it a string that
+represents your Email Address(e.g. example@example.com).
+Show the blow mentioned message in an alert box.(Hint: use
+string concatenation)*/
+// Answer
+// 
+alert("g.babar.khan@gmail.com")
+alert String conctenation=
