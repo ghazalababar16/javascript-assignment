4bd8924e728d6ad9ff0ffdae0de509ef0bb26034
ghazalababar16 <g.babar.khan@gmail.com>
2025-09-04 15:44:44 +0500

    save changes

--- assignment-02/app.js
+++ assignment-02/app.js
@@ -1,12 +1,15 @@
 //  1. Declare a variable called username.
 // answer
-// var  studentName="ghazala babar"
+// var  userName=
 
 /*2. Declare a variable called myName & assign to it a string
 that represents your Full Name.*/
-// 
-/*console . log("studentName")
-console .log("ghazala babar")*/
+// answer
+
+// var myName=("ghazalababar")
+// console.log(myName)
+
+
 
 /*3. Write script to
 a) Declare a JS variable, titled message.
@@ -14,27 +17,29 @@ b) Assign “Hello World” to variable message
 c) Display the message in alert box.*/
 
 // answer
-/*var massage="welcome to javascript"
-console.log ("massage")*/
-
 // var massage="hello world"
-// console.log ("hello world")
+// alert (massage)
 
 // alert("welcome to javascript")
 
 /*4. Write a script to save student’s bio data in JS variables and
 show the data in alert boxes.*/
 // answer
+// var studentName=("ghazala")
+// alert(studentName)
 
-/*alert ("ghazala babar")
-alert(" 34 years old")
-alert("certified website development")*/
+// var age=("34")
+// alert(age)
+
+// var studentCourse=("web & app development")
+// alert(studentCourse)
 
 /*5. Write a script to display the following alert using one JS
 variable:*/
 // answer
 
-// alert("PIZZA \nPIZZ\nPIZ \nPI \nP")
+// var food=("PIZZA \nPIZZ\nPIZ \nPI \nP")
+// alert(food)
 
 
 /*6. Declare a variable called email and assign to it a string that
@@ -42,8 +47,9 @@ represents your Email Address(e.g. example@example.com).
 Show the blow mentioned message in an alert box.(Hint: use
 string concatenation)*/
 // Answer
-// 
-// alert("g.babar.khan@gmail.com")
+
+// var email=("g.babar.khan@gmail.com")
+// alert(email)
 
 
 
@@ -57,10 +63,10 @@ message in an alert box:*/
 
 /*8. Write a script to display this in browser through JS*/
 // answer
- document.write("Yah!I can write HTML content  through javascript")
+//  document.write("Yah!I can write HTML content  through javascript")
 
  /*9. Store following string in a variable and show in alert and
 browser through JS*/
 
 // answer
-alert("----------THE END-----------")
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
